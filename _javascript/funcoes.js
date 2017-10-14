04a63359a418b89d50dc8c978c987b83d5f3a707
britocoder@outlook.com <alexandrebrito.info>
2017-10-14 21:03:39 +0100

    segundo commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,15 @@
+function mudaFoto(foto)
+	{
+		document.getElementById("icone").src= foto;
+	}
+	
+	
+function main() {
+  
+  $('.container').hide(); 
+  $('.container').fadeIn(1000);
+  
+}
+
+$(document).ready(main);
+
